f8f96b9b35ffaec28f440ba333fb6e10a552a43b
Ricks <bricks@unomaha.edu>
2020-09-21 12:38:35 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main(){
+        BinaryTree<String> binaryTree = new BinaryTree();
+        binaryTree.add("mike");
+        binaryTree.add("golf");
+        binaryTree.add("sierra");
+    }
+}
